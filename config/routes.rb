5da3d4e0bc7729d8ecb6c5e8270fd44b3ff9bce0
yuanqi <gzcyyq@gmail.com>
2019-05-30 21:14:02 +0900

    implemented thread from API to MongoDB for resource `States`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope "/api", defaults: {format: :json} do
     resources :cities, excpet: [:new, :edit]
+    resources :states, excpet: [:new, :edit]
     end
   root "ui#index"
 end
