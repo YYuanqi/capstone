65b0dd80df898a0a776e6d5e8512216417175c34
Yun Yuanqi <gzcyyq@gmail.com>
2019-07-10 15:11:12 +0000

    city before/after blocks

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe City, type: :model do
 
-  it "created City will be persisted, have a name, and be found" do
-    city = City.create(:name => "test");
-    expect(city).to be_persisted;
-    expect(city.name).to eq("test")
-    expect(City.find(city.id)).to_not be_nil
+  context "create City" do
+    before(:each) do
+      @city = City.create(:name => "test")
+    end
+    
+    after(:each) do
+      @city.delete
+    end
+    
+    it "will be persisted, have a name, and be found" do
+      expect(@city).to be_persisted;
+      expect(@city.name).to eq("test")
+      expect(City.find(@city.id)).to_not be_nil
+    end
   end
 end
 
