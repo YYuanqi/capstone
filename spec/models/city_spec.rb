48a10aae0d670a16e5dd51a77a8acf1c36100b6d
Yun Yuanqi <gzcyyq@gmail.com>
2019-07-11 14:19:21 +0000

    added db cleaner shared contexts

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
+  include_context "db_cleanup", :transaction
+  include_context "db_scope"
 
   context "create City" do
-    before(:each) do
-      @city = City.create(:name => "test")
+    let(:city) { City.create(:name => "test") }
+    
+    it "will be persist" do
+      expect(city).to be_persisted
     end
     
-    after(:each) do
-      @city.delete
+    it "have a name" do
+      expect(city.name).to eq("test")
     end
     
-    it "will be persisted, have a name, and be found" do
-      expect(@city).to be_persisted;
-      expect(@city.name).to eq("test")
-      expect(City.find(@city.id)).to_not be_nil
+    it "will be found" do
+      expect(City.find(city.id)).to_not be_nil
     end
   end
 end
