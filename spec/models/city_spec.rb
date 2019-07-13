f6e8137102e1e32f951dcd154effdded9d0f03d4
YYuanqi <gzcyyq@gmail.com>
2019-07-13 17:27:26 +0900

    imported database clean strategy to city_sepc

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe City, type: :model do
+describe City, type: :model do
   include_context "db_cleanup", :transaction
-  include_context "db_scope"
+  before(:all) do
+    @city=City.create(:name=>"test")
+  end
+  let(:city) { City.find(@city.id) }
 
-  context "create City" do
-    let(:city) { City.create(:name => "test") }
-    
-    it "will be persist" do
-      expect(city).to be_persisted
-    end
-    
-    it "have a name" do
-      expect(city.name).to eq("test")
-    end
-    
-    it "will be found" do
-      expect(City.find(city.id)).to_not be_nil
-    end
+  context "created city (let)" do
+    it { expect(city).to be_persisted }
+    it { expect(city.name).to eq("test") }
+    it { expect(City.find(city.id)).to_not be_nil }
+  end
+
+  context "created city (subject)" do
+    subject { @city }
+
+    it { is_expected.to be_persisted }
+    it { expect(subject.name).to eq("test") }
+    it { expect(City.find(subject.id)).to_not be_nil }
   end
 end
-
