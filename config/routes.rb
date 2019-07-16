b17ee09484f11a412508de098b9a99d7fb2d5033
yuanqi <gzcyyq@gmail.com>
2019-07-17 00:05:05 +0900

    added image resouce routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   scope "/api", defaults: {format: :json} do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
+    resources :images, except: [:new, :edit]
     end
   root "ui#index"
 end
