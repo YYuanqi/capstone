eb5469fa9ef57a6477500c2b21708c864333c3ed
yuanqi <gzcyyq@gmail.com>
2019-07-16 23:54:31 +0900

    added image factory

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :image do
-    caption { "MyString" }
-    creator_id { "MyString" }
+    sequence(:caption) { |n| n % 2 == 0 ? nil : Faker::Lorem.sentence(1).chomp(".") }
+    creator_id { 1 }
+
+    trait :with_caption do
+      caption { Faker::Lorem.sentence(1).chomp(".") }
+    end
   end
 end
