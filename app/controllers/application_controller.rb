2bf100e334b67eb98dda57861f1e4c16835de124
yuanqi <gzcyyq@gmail.com>
2019-07-21 15:34:52 +0900

    added thing and thing-authn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_144118) do
+ActiveRecord::Schema.define(version: 2019_07_21_043512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,19 @@ ActiveRecord::Schema.define(version: 2019_07_16_144118) do
 
   create_table "images", force: :cascade do |t|
     t.string "caption"
-    t.string "creator_id", null: false
+    t.integer "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["creator_id"], name: "index_images_on_creator_id"
+  end
+
+  create_table "things", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_things_on_name"
   end
 
   create_table "users", force: :cascade do |t|
