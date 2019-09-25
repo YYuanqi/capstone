fd4feb328cf904d25b61d9d5008efb6dc5d00256
yuanqi <gzcyyq@gmail.com>
2019-09-25 21:48:18 +0900

    added login helper method

--- spec/support/ui_helper.rb
+++ spec/support/ui_helper.rb
@@ -11,6 +11,22 @@ module UiHelper
     end
   end
 
+  def login credentials
+    visit root_path
+    find("#navbar-loginlabel", text: "Login").click
+    within("#login-form") do
+      fill_in("login_email", with: credentials[:email])
+      fill_in("login_password", with: credentials[:password])
+      click_button("Login")
+    end
+    using_wait_time 5 do
+      expect(page).to have_no_css("#login-form")
+    end
+
+    expect(page).to have_css("#logout-form", visible: false)
+    expect(page).to have_css("#navbar-loginlabel", text: /#{credentials[:name]}/)
+  end
+
   def fillin_signup registration
     visit "#{ui_path}/#/signup" unless page.has_css?("#signup-form")
     using_wait_time 5 do
