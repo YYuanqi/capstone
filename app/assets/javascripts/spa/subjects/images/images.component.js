42f7caf2b5d8466710fb52e8d403430d107d9ccf
yuanqi <gzcyyq@gmail.com>
2019-10-05 20:04:39 +0900

    display image impl

--- app/assets/javascripts/spa/subjects/images/images.component.js
+++ app/assets/javascripts/spa/subjects/images/images.component.js
@@ -6,7 +6,12 @@
     .component("sdImageSelector", {
       templateUrl: imageSelectorTemplateUrl,
       controller: ImageSelectorController,
-      // bindings: {}
+      bindings: {}
+    })
+    .component("sdImageEditor", {
+      templateUrl: imageEditorTemplateUrl,
+      controller: ImageEditorController,
+      binding: {}
     });
 
   imageSelectorTemplateUrl.$inject = ["spa.config.APP_CONFIG"];
@@ -15,18 +20,46 @@
     return APP_CONFIG.image_selector_html;
   }
 
+  imageEditorTemplateUrl.$inject = ["spa.config.APP_CONFIG"];
+
+  function imageEditorTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.image_editor_html;
+  }
+
   ImageSelectorController.$inject = ["$scope",
+    "$stateParams",
+    "spa.subjects.Image"];
+
+  ImageEditorController.$inject = ["$scope",
     "$stateParams",
     "spa.subjects.Image"];
 
   function ImageSelectorController($scope, $stateParams, Image) {
     var vm = this;
 
-    vm.$onInit = function() {
+    vm.$onInit = function () {
       console.log("ImageSelectorController", $scope);
       if (!$stateParams.id) {
         vm.items = Image.query();
       }
+    }
+  }
+
+  function ImageEditorController($scope, $stateParams, Image) {
+    var vm = this;
+
+    vm.$onInit = function () {
+      console.log("ImageEditorController", $scope);
+      if ($stateParams.id) {
+        vm.item = Image.get({id: $stateParams.id});
+        console.log(new Image());
+      } else {
+        newResource();
+      }
+    };
+
+    function newResource() {
+      vm.item = new Image();
     }
   }
 })();
