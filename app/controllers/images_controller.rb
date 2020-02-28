0c8ca65b2a5c3f0cdd9c4bb0d0434884bc964b86
yuanqi <gzcyyq@gmail.com>
2020-02-28 16:08:50 +0900

    added image size queries

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,7 +16,7 @@ class ImagesController < ApplicationController
   end
 
   def content
-    result = ImageContent.image(@image).smallest.first
+    result = ImageContent.image(@image).smallest(params[:width], params[:height]).first
     if result
       options = {
         type: result.content_type,
