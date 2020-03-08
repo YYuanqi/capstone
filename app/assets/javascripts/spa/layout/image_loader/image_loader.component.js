0d952e37cc457c33c1643ceed1dce8061084f949
yyq <gzcyyq@gmail.com>
2020-03-08 18:15:44 +0900

    added ui-cropper and image edit

--- app/assets/javascripts/spa/layout/image_loader/image_loader.component.js
+++ app/assets/javascripts/spa/layout/image_loader/image_loader.component.js
@@ -2,7 +2,7 @@
   "use strict";
 
   angular
-    .module("spa.layout")
+    .module("spa-demo.layout")
     .component("sdImageLoader", {
       templateUrl: templateUrl,
       controller: ImageLoaderController,
@@ -13,54 +13,56 @@
     });
 
 
-  templateUrl.$inject = ["spa.config.APP_CONFIG"];
+  templateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
 
   function templateUrl(APP_CONFIG) {
     return APP_CONFIG.image_loader_html;
   }
 
-  ImageLoaderController.$inject = ["$scope", "UploadDataUrl"];
+//  ImageLoaderController.$inject = ["$scope","UploadDataUrl"];
+//  function ImageLoaderController($scope, UploadDataUrl) {
 
-  function ImageLoaderController($scope, UploadDataUrl) {
+  ImageLoaderController.$inject = ["$scope"];
+
+  function ImageLoaderController($scope) {
     var vm = this;
     vm.debug = debug;
 
     vm.$onInit = function () {
       console.log("ImageLoaderController", $scope);
       $scope.$watch(function () {
-          return vm.file
+          return vm.dataUri
         },
         function () {
-          makeObjectUrl();
-          makeDataUri();
+          vm.resultDataUri({dataUri: vm.dataUri});
         });
-    };
+      // $scope.$watch(function(){ return vm.file },
+      //               function(){ makeObjectUrl(); makeDataUri(); });
+    };t
     return;
-
     //////////////
-    function makeDataUri() {
-      vm.dataUri = null;
-      if (vm.file) {
-        UploadDataUrl.dataUrl(vm.file, true).then(
-          function (dataUri) {
-            vm.dataUri = dataUri;
-            console.log("created dataUri", vm.file, vm.dataUri.length);
-            vm.resultDataUri({dataUri: vm.dataUri})
-          });
-      }
-    }
+    // function makeDataUri() {
+    //   vm.dataUri=null;
+    //   if (vm.file) {
+    //     UploadDataUrl.dataUrl(vm.file, true).then(
+    //       function(dataUri){
+    //         vm.dataUri = dataUri;
+    //         console.log("created dataUri", vm.file, vm.dataUri.length);
+    //         vm.resultDataUri({dataUri: vm.dataUri})
+    //       });
+    //   }
+    // }
 
-    function makeObjectUrl() {
-      vm.objectUrl = null;
-      if (vm.file) {
-        UploadDataUrl.dataUrl(vm.file, false).then(
-          function (objectUrl) {
-            vm.objectUrl = objectUrl;
-            console.log("created objectURL", vm.file, vm.objectUrl);
-          });
-      }
-    }
-
+    // function makeObjectUrl() {
+    //   vm.objectUrl = null;
+    //   if (vm.file) {
+    //     UploadDataUrl.dataUrl(vm.file, false).then(
+    //       function(objectUrl){
+    //         vm.objectUrl = objectUrl;
+    //         console.log("created objectURL", vm.file, vm.objectUrl);
+    //       });
+    //   }
+    // }
     function debug() {
       console.log("ImageLoaderController", $scope);
     }
